9114574afc2baa6e30c5431ef12be57bad3a65a6
woniu <94002762+Snail-one@users.noreply.github.com>
2025-03-27 23:21:13 +0800

    增加tcp

--- scripts/ipv6_forward.sh
+++ scripts/ipv6_forward.sh
@@ -7,6 +7,8 @@ IPV4_GAME_PORT="${SERVER_PORT:-8211}"
 # 启用 IPv6 端口转发
 echo "Starting IPv6 to IPv4 port forwarding..."
 socat UDP6-LISTEN:${IPV6_GAME_PORT},ipv6-v6only=1,fork UDP4:127.0.0.1:${IPV4_GAME_PORT} &
+# 加入 tcp 端口转发
+socat TCP6-LISTEN:${IPV6_GAME_PORT},ipv6-v6only=1,fork TCP4:127.0.0.1:${IPV4_GAME_PORT} &
 
 # 保持脚本运行
 wait
